b317a359d0fcaf3d2abc851fa178100c10afb8b6
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-04-12 14:50:53 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,14 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./mesan.db")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
-)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
 def init_db():
     from models import Lead
     Base.metadata.create_all(bind=engine)
-
-def conectar():
-    return SessionLocal()
-    conn.close()
